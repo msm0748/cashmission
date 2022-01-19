d10be5a4c3bb09c237c79b2ba427b98276b159f5
Moon-Seokmin <msm0748@gmail.com>
2022-01-19 20:19:38 +0900

    last commit

--- cashmission.js
+++ cashmission.js
@@ -9,7 +9,7 @@ document.onmousemove = (e) => {
 //원하는 위치에 마우스 커서를 두고 아래 코드 입력
 console.log(`${x}, ${y}`);
 
-/* 글자 잘 베껴썼는지 검사하기 (F11 후 크기 75%) */
+/* 글자 잘 베껴썼는지 검사하기 (F11 후 크기 50%) */
 
 function simulateClick(x, y) {
   var evt = document.createEvent("MouseEvents");
@@ -35,20 +35,21 @@ function simulateClick(x, y) {
 }
 
 window.onkeydown = (e) => {
-  if (e.keyCode == 49) {
-    simulateClick(2003, 147);
-  } else if (e.keyCode == 50) {
-    simulateClick(2005, 341);
-  } else if (e.keyCode == 51) {
-    simulateClick(2006, 391);
-  } else if (e.keyCode == 52) {
-    simulateClick(2006, 439);
-  } else if (e.keyCode == 53) {
-    simulateClick(2005, 499);
-  } else if (e.keyCode == 192) {
-    simulateClick(1754, 1110);
-  } else if (e.keyCode == 32) {
-    simulateClick(1897, 1110);
+  var textarea = document.querySelector(
+    "#app > div.MuiBox-root.jss6 > div > div.MuiBox-root.jss8 > div > div > div.LabelingValidationPanel__Container-sc-8zanhw-1.hLcnQB > div:nth-child(7) > div.LabelingInputItemContainer__TextAnswer-sc-15f33n6-0.fwbwmL > textarea"
+  );
+  if (e.key == "1") {
+    simulateClick(3030, 144);
+  } else if (e.key == "2") {
+    simulateClick(3030, 396);
+  } else if (e.key == "3") {
+    simulateClick(3030, 590);
+  } else if (e.key == "+") {
+    simulateClick(2801, 1672);
+  } else if (e.key == "Enter") {
+    simulateClick(2955, 1692);
+  } else if (e.key == "0") {
+    textarea.style.fontSize = "32px";
   }
 };
 
@@ -76,24 +77,35 @@ function simulateClick(x, y) {
   var cb = document.elementFromPoint(x, y);
   cb.dispatchEvent(evt);
 }
-
-window.onkeydown = (e) => {
+function focus() {
   var textarea = document.querySelector(
-    "#app > div > div.MissionContainer__WorkContainer-sc-1f05koo-1.ZcmPw > div > div.LabelingImageTaskRightBar__Container-sc-fubl6n-0.jYYhLO > div.LabelingRightContent__Container-sc-ui9idm-0.elQYbQ > div > div.Pane.horizontal.Pane2 > div > div > div.LabelingPropertyContainer__ItemContainer-sc-1eqfn6b-3.kdaPMj > div:nth-child(4) > div.LabelingInputItemContainer__TextAnswer-sc-15f33n6-0.fwbwmL > textarea"
+    "#app > div > div.MissionContainer__WorkContainer-sc-1f05koo-1.ZcmPw > div > div.LabelingImageTaskRightBar__Container-sc-fubl6n-0.jYYhLO > div.LabelingRightContent__Container-sc-ui9idm-0.elQYbQ > div > div.Pane.horizontal.Pane2 > div > div > div.LabelingPropertyContainer__ItemContainer-sc-1eqfn6b-3.iUfbbT > div:nth-child(4) > div.LabelingInputItemContainer__TextAnswer-sc-15f33n6-0.fwbwmL > textarea"
   );
-  if (e.keyCode == 32) {
-    simulateClick(3049, 920);
-    simulateClick(3049, 1193);
-    simulateClick(3049, 1419);
-  } else if (e.keyCode == 17) {
-    simulateClick(3049, 920);
-    simulateClick(3049, 1248);
-    simulateClick(3049, 1419);
-  }
   setTimeout(() => {
     textarea.style.fontSize = "32px";
     textarea.focus();
   }, 200);
+}
+window.oncontextmenu = (e) => {
+  e.preventDefault();
+  simulateClick(3049, 966);
+  simulateClick(3049, 1193);
+  simulateClick(3049, 1419);
+  focus();
+};
+
+window.onkeydown = (e) => {
+  if (e.keyCode == 32) {
+    simulateClick(3049, 920);
+    simulateClick(3049, 1193);
+    simulateClick(3049, 1419);
+    focus();
+  } else if (e.keyCode == 17 && e.keyCode == 16) {
+    simulateClick(3049, 920);
+    simulateClick(3049, 1248);
+    simulateClick(3049, 1419);
+    focus();
+  }
 };
 
 /* 악플 75% */
